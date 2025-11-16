fc1754b17cb14718eece8586ecf66fa14492bcdd
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-11-16 11:40:16 +0530

    signup page working

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -23,21 +23,21 @@ const SignUp = () => {
         <div className='mb-2'>
           <label htmlFor="fullName" className='block text-gray-700 font-semibold mb-1'>Full Name</label>
           <input type='text' placeholder='Enter your full name' id="fullName"
-            className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
+            className="w-full p-2 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
             style={{ border: `1px solid ${borderColor}` }} />
         </div>
 
         <div className='mb-2'>
           <label htmlFor="email" className='block text-gray-700 font-semibold mb-1'>Email</label>
           <input type="email" placeholder="Email"
-            className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
+            className="w-full p-2 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
             style={{ border: `1px solid ${borderColor}` }} />
         </div>
 
         <div className='mb-2'>
           <label htmlFor="mobile" className='block text-gray-700 font-semibold mb-1'>Mobile Number</label>
           <input type='text' placeholder='Enter your mobile number' id="mobile"
-            className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
+            className="w-full p-2 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
             style={{ border: `1px solid ${borderColor}` }}>
           </input>
         </div>
@@ -45,13 +45,13 @@ const SignUp = () => {
         <div className='mb-2'>
           <label htmlFor="password" className='block text-gray-700 font-semibold mb-1'>Password</label>
           <div className='relative'>
-            <input type='password' placeholder='Password'
-              className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1" />
-            <button className='absolute right-3 top-3.5 text-gray-500' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ? <FaEye /> : <FaEyeSlash />}</button>
+            <input type={`${!showPassword ? 'text' : 'password'}`} placeholder='Password'
+              className="w-full p-2 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1" />
+            <button className='absolute right-3 top-2.5 text-gray-500' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ? <FaEye /> : <FaEyeSlash />}</button>
           </div>
         </div>
 
-        <button className={`w-full p-3 rounded text-white font-semibold cursor-pointer hover:opacity-90 transition`}
+        <button className={`w-full p-2 rounded text-white font-semibold cursor-pointer hover:opacity-90 transition`}
           style={{ backgroundColor: primaryColor }}
         >
           Register
