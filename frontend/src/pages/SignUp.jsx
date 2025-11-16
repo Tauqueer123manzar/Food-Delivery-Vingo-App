7ee159b7ba69fe3da900f14006725a0313932c30
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-11-16 11:00:12 +0530

    singup page working

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useState } from 'react';
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
   const primaryColor = "#ff4d2d";
@@ -6,34 +9,58 @@ const SignUp = () => {
   const bgColor = "#fff9f6";
   const borderColor = "#ddd";
 
+  const [showPassword, setShowPassword] = useState(true);
+
   return (
     <div className='min-h-screen w-full flex items-center justify-center p-4'
-      style={{ backgroundColor: bgColor}}>
-     <div className = {`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-$[1px]`}
-     style={{border: `1px solid ${borderColor}`}} >
-      <h1 className={`text-3xl font-bold mb-2 text-center`} style={{color:primaryColor}}>Vingo</h1>
+      style={{ backgroundColor: bgColor }}>
+      <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-$[1px]`}
+        style={{ border: `1px solid ${borderColor}` }} >
+        <h1 className={`text-3xl font-bold mb-2 text-center`} style={{ color: primaryColor }}>Vingo</h1>
 
-       <input type='text' placeholder='Username'
-        className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
-        />
+        <p className='text-gray-600 mb-8'>Create your account to get started with delicious food deliveries</p>
 
-       <input type="email" placeholder="Email"
-        className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
-       />
+        <div className='mb-2'>
+          <label htmlFor="fullName" className='block text-gray-700 font-semibold mb-1'>Full Name</label>
+          <input type='text' placeholder='Enter your full name' id="fullName"
+            className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
+            style={{ border: `1px solid ${borderColor}` }} />
+        </div>
 
-       <input type='password' placeholder='Password'
-        className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"/>
+        <div className='mb-2'>
+          <label htmlFor="email" className='block text-gray-700 font-semibold mb-1'>Email</label>
+          <input type="email" placeholder="Email"
+            className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
+            style={{ border: `1px solid ${borderColor}` }} />
+        </div>
 
-      <button className={`w-full p-3 rounded text-white font-semibold cursor-pointer hover:opacity-90 transition`}
-         style={{backgroundColor: primaryColor}}
+        <div className='mb-2'>
+          <label htmlFor="mobile" className='block text-gray-700 font-semibold mb-1'>Mobile Number</label>
+          <input type='text' placeholder='Enter your mobile number' id="mobile"
+            className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
+            style={{ border: `1px solid ${borderColor}` }}>
+          </input>
+        </div>
+
+        <div className='mb-2'>
+          <label htmlFor="password" className='block text-gray-700 font-semibold mb-1'>Password</label>
+          <div className='relative'>
+            <input type='password' placeholder='Password'
+              className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1" />
+            <button className='absolute right-3 top-3.5 text-gray-500' onClick={() => setShowPassword(prev => !prev)}>{!showPassword ? <FaEye /> : <FaEyeSlash />}</button>
+          </div>
+        </div>
+
+        <button className={`w-full p-3 rounded text-white font-semibold cursor-pointer hover:opacity-90 transition`}
+          style={{ backgroundColor: primaryColor }}
         >
           Register
         </button>
 
         <a href='/signin' className='block text-center mt-4 text-sm text-gray-600'>
           Already have an account? Sign In
-        </a>  
-     </div>
+        </a>
+      </div>
     </div>
   )
 }
