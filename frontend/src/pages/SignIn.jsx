9675812a0fad519c1ec295fea533301bad8a0eca
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-12-19 20:16:37 +0530

    code added in the signin section

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -14,13 +14,13 @@ const SignIn = () => {
 
   return (
     <>
-    <div className='min-h-screen w-full flex items-center justify-center p-4'
+    <div className='min-h-screen w-full flex items-center justify-center p-4 mt-16'
       style={{ backgroundColor: bgColor }}>
       <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-$[1px]`}
         style={{ border: `1px solid ${borderColor}` }} >
         <h1 className={`text-3xl font-bold mb-2 text-center`} style={{ color: primaryColor }}>Vingo</h1>
         <p className='text-gray-700 mb-8 text-'>Welcome back! Please signin to continue enjoying delicious food deliveries.</p>
-        
+
         <label htmlFor='email' className='block text-gray-700 font-semibold mb-1'>Email</label>
         <input type="email" placeholder="Email"
           className="w-full p-3 mb-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-1"
@@ -55,7 +55,7 @@ const SignIn = () => {
             </button>
         </div>
         <a href='/signup' className='block text-center mt-4 text-sm text-gray-600'>
-         If you have no account then please ? <span className='text-blue-600 font-medium'>Sign Up</span>
+         If you have no account then?<span className='text-blue-600 font-medium'>SignUp</span>
         </a>
       </div>
     </div>
