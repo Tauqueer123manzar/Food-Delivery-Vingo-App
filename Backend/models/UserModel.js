9aad6edb0b6eabbea2958a690ad733a09d2be428
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-12-20 19:41:40 +0530

    UserModel code added

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -1,5 +1,29 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    
-})
+    fullName : {
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+    },
+    mobile:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['user','owner','deliveryBoy'],
+        required:true
+    }
+},{timestamps:true});
+
+const User = mongoose.model('User',UserSchema);
+
+export default User;
