6b0c62ae029f3a176f720a13e8f8ba7f1f2084e1
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-12-21 11:55:19 +0530

    code added

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -1,6 +1,6 @@
 import User from "../models/UserModel";
 import bcrypt from "bcryptjs";
-
+import genToken from '../utils/token';
 // ==================================== Signup ====================================
 export const signup=async(req,res)=>{
     try{
@@ -34,6 +34,19 @@ export const signup=async(req,res)=>{
         mobile,
         password:hashedPassword,
         role
+     });
+     const token = genToken(newUser._id);
+     res.cookie("token",token,{
+        httpOnly : true,
+        secure: false,
+        sameSite: "strict",
+        maxAge: 7*24*60*60*1000 // 7 days
+     });
+    
+     res.status(201).json({
+        message:"User registered successfully",
+        newUser,
+        token
      });
 
     }catch(error){
